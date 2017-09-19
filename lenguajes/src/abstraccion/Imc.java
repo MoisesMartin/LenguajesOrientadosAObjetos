c180ea4e8e33f4fe7a54bc046ca022cd227057cd
T-102 <T-102@PC241>
2017-09-18 19:40:08 -0500

    Clase 3 finalizada

--- lenguajes/src/abstraccion/Imc.java
+++ lenguajes/src/abstraccion/Imc.java
@@ -4,4 +4,11 @@ public class Imc
 {
     Usuario usr;
     String resultado;
+    
+    public String calcularIMC()
+    {
+        float valorIMC = usr.peso/(usr.altura*usr.altura);
+        resultado = "Tu IMC es: "+valorIMC;
+        return resultado;
+    }
 }
